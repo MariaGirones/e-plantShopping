e8ae3d59f936535efb564364bc56f5bfec29f825
MariaGirones <mariagirones.me@gmail.com>
2025-08-22 15:48:20 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e-plantShopping/'
+  base: '/e-plantShopping/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
